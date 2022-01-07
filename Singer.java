47cc4f6182077d467247719bd7db36f0198df171
Tony Zheng <82166548+tonyz-1@users.noreply.github.com>
2022-01-07 11:29:47 -0700

    Update Singer.java

--- Singer.java
+++ Singer.java
@@ -1,11 +1,8 @@
-//Tony Zheng 20187099
 
 public class Singer extends Person {
-	//these two instance variables were required
 	private String debutAlbum;
 	private Date debutAlbumReleaseDate;
 	
-	//this constructor was required, and needed to avoid privacy leaks
 	public Singer(String name, Date birthday, String debutAlbum, Date debutAlbumReleaseDate) {
 		super(name, birthday);
 		this.debutAlbum = debutAlbum;
@@ -18,12 +15,10 @@ public class Singer extends Person {
 		return newSinger;
 	}
 	
-	//this toString method was required
 	public String toString() {
 		return super.toString()+". Their debut album was "+this.debutAlbum+", and it was released on "+this.debutAlbumReleaseDate.getDay()+" "+this.debutAlbumReleaseDate.getMonth()+", "+this.debutAlbumReleaseDate.getYear();
 	}
 	
-	//this method was required
 	public String personType() {
 		return "singer";
 	}
