43bf1d1836ace051497f95060bf1cc01f85dcf85
Tony Zheng <82166548+tonyz-1@users.noreply.github.com>
2022-01-07 11:29:17 -0700

    Update Politician.java

--- Politician.java
+++ Politician.java
@@ -1,14 +1,10 @@
-//Tony Zheng 20187099
 
 public class Politician extends Person{
-	//this one instance variable was required
 	private String party;
 	
-	//this constructor was required, and needed to avoid privacy leaks
 		public Politician(String name, Date birthday, String party) {
 			super(name, birthday);
 			this.party = party;
-			
 		}
 
 		//better clone method
@@ -17,12 +13,10 @@ public class Politician extends Person{
 			return newPolitician;
 		}
 		
-		//this toString method was required
 		public String toString() {
 			return super.toString()+". They are a member of the "+this.party+" party.";
 		}
 		
-		//this method was required
 		public String personType() {
 			return "politician";
 		}
